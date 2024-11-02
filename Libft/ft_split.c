c3ff6ff17630d157de6aaa4eae124227ad27b4e2
Paul Lerick <plerick@c2r7p3.42lehavre.fr>
2024-11-02 22:29:28 +0100

    avance split

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: plerick <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 22:51:38 by plerick           #+#    #+#             */
-/*   Updated: 2024/11/01 04:13:39 by plerick          ###   ########.fr       */
+/*   Updated: 2024/11/02 22:22:29 by plerick          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,52 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+static	char	*ft_strcpy(char *tabg, char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s != c || *s == s[0])
+	{
+		i++;
+		s--;
+	}
+	if (*s == s[0])
+	{
+		i++;
+		while (i-- > 0)
+			*tabg++ = *s++;
+	}
+	else
+	{
+		s++;
+		while (i-- > 0)
+			*tabg++ = *s++;
+	}
+	*tabg = '\0';
+	return (tabg);
+}
+
+static size_t	ft_word(char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s != c || *s == s[0])
+	{
+		i++;
+		s--;
+	}
+	if (*s == s[0])
+		i++;
+	return (i);
+}
 
 static int	ft_count(char const *s, char c)
 {
-	size_t	count;
+	int	count;
 	size_t	i;
 
 	count = 1;
@@ -40,21 +82,45 @@ static int	ft_count(char const *s, char c)
 char	**ft_split(char const *s, char c)
 {
 	size_t	nb_word;
+	char	**tabg;
+	int	i;
 
+	i = 0;
 	if (*s == '\0' || c == '\0')
 		return (NULL);
-	nb_words = (ft_count(s, c));
-	return (0);
+	nb_word = (ft_count(s, c));
+	tabg = (char **)malloc(sizeof(char *) * (nb_word + 1));
+	if (tabg == NULL)
+		return (NULL);
+	while (*s)
+	{
+		if ((*s != c) && ((*s + 1) == (int)c || (*s + 1) == '\0'))
+		{
+			tabg[i] = (char *)malloc(sizeof(char) * ft_word(s, c) + 1);
+			if (tabg[i] == NULL)
+				{
+					while (i-- >= 0)
+						free(tabg[i]);
+					return (NULL);
+				}
+			tabg[i] = ft_strcpy(tabg[i], s, c);
+			i++;
+		}
+		s++;
+	}
+	tabg[i] = NULL;
+	return (tabg);
 }
 
 int	main(void)
 {
-	char const s[] = "-------Bonjour-je-suis-un-pc----";
+	char const s[] = "Bonjour-je-suis-un---pc";
 	char	c = '-';
-	char new_str;
+	char **new_str;
 
 //	printf("%d\n", ft_count(s, c));
 	new_str = ft_split(s, c);
-	printf("%s\n", new_str);
+	printf("%s\n", new_str[1]);
+	free(new_str);
 	return (0);
 }
