fafd185916cb2b76de96d03ad0071cfe348d4599
Paul Lerick <plerick@c2r8p3.42lehavre.fr>
2024-11-09 00:57:47 +0100

    avance printf

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plerick <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 19:17:28 by plerick           #+#    #+#             */
+/*   Updated: 2024/11/07 22:15:04 by plerick          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+int	my_check(va_list args, const char *str)
+{
+	int	i;
+	int	fd;
+	
+	i = 0;
+	fd = 1;
+	if (str[i + 1] == 'c')
+		ft_putchar_fd(va_arg(args, int), fd);
+	else if (str[i + 1] == 's')
+		ft_putstr_fd(va_arg(args, char *), fd);
+	else if (str[i + 1] == 'd')
+		ft_putnbr_fd(va_arg(args, int), fd);
+
+
+	else
+		{
+			ft_putchar_fd(str[i], fd);
+			if (str[i + 1] == '\0')
+				ft_putchar_fd(str[i + 1], fd);
+		}
+	return 0;
+}
+
+int	ft_printf(const char *str, ...)
+{
+	int	i;
+	int	fd;
+	
+	i = 0;
+	fd = 1;
+	va_list args;
+	va_start(args, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			my_check(args, &str[i]);
+			i++;
+		}
+		else
+			ft_putchar_fd(str[i], fd);
+		i++;
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	int	i = 5;
+	int	a = 'g';
+	char s[] = "Haha";
+
+	ft_printf("%c %s, % j'ai %d !%", a, s, i);
+	return (0);
+}
---
+++ ft_printf/libftprintf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plerick <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 19:14:08 by plerick           #+#    #+#             */
+/*   Updated: 2024/11/07 19:16:13 by plerick          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include "libft/libft.h"
+# include <stdarg.h>
+
+int	ft_printf(const char *str, ...);
+
+
+#endif
