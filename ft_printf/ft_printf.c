b9902a3b995cbd9c678115abb1ede64e983dad9a
Paul Lerick <plerick@c2r8p3.42lehavre.fr>
2024-11-16 22:40:09 +0100

    printptr almost ok

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: plerick <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 19:17:28 by plerick           #+#    #+#             */
-/*   Updated: 2024/11/07 22:15:04 by plerick          ###   ########.fr       */
+/*   Updated: 2024/11/16 22:22:14 by plerick          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int	my_check(va_list args, const char *str)
 		count += ft_printbase_lo(va_arg(args, long));
 	else if (str[i + 1] == 'X')
 		count += ft_printbase_up(va_arg(args, long));
-	else if (str[i + 1] == 'p')
+ 	else if (str[i + 1] == 'p')
 		count += ft_printptr(va_arg(args, void*));	
 		
 	else
@@ -78,13 +78,15 @@ int	main(void)
 //	char s[] = "Haha";
 //	int	iii = 456;
 	int	count = 0;
-	long	base1 = 255;
-	long	base2 = 4095;
-	
-	
+//	long	base1 = 1;
+//	long	base2 = '\0';
+	int	nbptr = 1;
+	int	*ptr = &nbptr;
+
 //	count = ft_printf("%c %s, % j'vai %d !%% % wooooow plein de chiffre %i\n", a, s, i, iii);
 //	count = ft_printf("%s\n", s);
-	count = ft_printf("%x\n%X\n", base1, base2);
+//	count = ft_printf("%s\n%X\n", (char *)NULL, base2);
+	count = ft_printf("%p", ptr);
 	ft_printf("%i", count);
 	return (count);
 }
--- ft_printf/ft_printptr.c
+++ ft_printf/ft_printptr.c
@@ -11,48 +11,50 @@
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+//#include <unistd.h>
+//#include <stdio.h>
 
 static void	my_putchar(char c)
 {
-		write(1, &c, 1);
-		return ;
+	write(1, &c, 1);
+	return ;
 }
 
 int	ft_printptr(void *ptr)
 {
 	int	i;
-	unsigned long	ptr2;
-
+	unsigned long	num;
+	char	temp[20];
+	int	n;	
+		
+	num = (unsigned long)ptr;
 	i = 0;
-	ptr2 = (unsigned long)ptr;
-	if (ptr2 < 0)
+	n = 0;
+	if (num == 0)
+		return (write (1, "0x0", 3));
+	while (num > 15)
 	{
-		write(1, "-", 1);
-		i++;
-		ptr2 = ptr2 * -1;
+		if ((num % 16) < 10)
+			temp[i++] = ((num % 16) + '0');
+		else if ((num % 16) > 10)
+			temp[i++] = ((num % 16) - 10 + 'a');
+		num = num / 16;
 	}
-	if (ptr2 >= 0 && ptr2 < 16)
+	write(1, "0x", 2);
+	while (i > 0)
 	{
-		if (ptr2 < 10)
-			my_putchar(ptr2 + '0');
-		else
-			my_putchar(ptr2 - 10 + 'a');
-		return (i = i + 1);
+		my_putchar(temp[i--]);
+		n++;
 	}
-	else
-	{
-		i = i + (ft_printptr((void *)ptr2 / 16));
-		ft_printptr((void *)ptr2 % 16);
-		return (i + 1);
-	}
+	return (n + 1);
 }
 /*
 int	main(void)
 {
 	long	nb;
 
-	nb = 4095;
-	printf("\n%d\n",ft_printbase_lo(nb));
+	nb = 0;
+	printf("\n%d\n",ft_printptr((long *)nb));
 	return (0);
 }
 */
