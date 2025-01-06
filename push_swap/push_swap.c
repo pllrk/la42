5c240d7e0bc3f8dc49db3bd076eb79344fbf6bcb
root <root@BonPC>
2025-01-06 15:10:32 +0100

    add push_swap

---
+++ push_swap/push_swap.c
@@ -0,0 +1,36 @@
+
+/*
+Ce qu'on me demande de faire :
+- Avec des variables données dans un ordre aléatoire, je dois être capable au travers de mon programme de les trier par ordre de croissant
+
+Limitation :
+- Je n'ai le droit que à deux piles de stack A et B
+- On me fournit un checker pour test le code 
+
+Ce que je dois faire pour le projet :
+
+- Voir comment utiliser le checker
+- Me renseigner sur les listes chainées (voir si c'et completement necessaire pour le tri)
+    - Pour une liste chainée il faut d'abord savoir ce qu'est une structure 
+- Créer le makefile
+- Faire le code
+    - Créer les stack A et B
+    - Récupérer les variables et créer une liste chainée (voir comment faire et le principe de fonctionnement)
+    - Mettre l'ensemble des variables dans le stack A pour commencer (possible de d'or et déjà les prétrier? (à voir comment le checker fonctionne))
+    - Créer une fonction pour gérer les erreurs et free aux cas ou
+    - Une fonction pour faire les différentes oppérations (ap etc)
+    - Une fonction pour trouver le chiffre le plus petit et le plus grand
+- Choisir avec quel type d'alghorytme de tri je vais utiliser
+- Faire passer les variables dans l'algo
+- Done
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a = {2,3,8,7,9,4,1,0,5,6};
+
+	
+}
