071564105768f8cd4ba57778243979ab1a45ecda
Paul Lerick <plerick@c1r6p12.42lehavre.fr>
2025-01-13 21:45:59 +0100

    struct cree + init

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -1,28 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plerick <plerick@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 18:47:16 by plerick           #+#    #+#             */
+/*   Updated: 2025/01/13 18:47:16 by plerick          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 /*
 Ce qu'on me demande de faire :
-- Avec des variables données dans un ordre aléatoire, je dois être capable au travers de mon programme de les trier par ordre de croissant
+- Avec des variables données dans un ordre aléatoire,
+	je dois être capable au travers de mon programme de les trier par ordre de croissant
 
 Limitation :
 - Je n'ai le droit que à deux piles de stack A et B
-- On me fournit un checker pour test le code 
+- On me fournit un checker pour test le code
 
 Ce que je dois faire pour le projet :
 
-- Inclure GNL de Lucas dans ma Libft + changer la dest du makefile après (voir pour faire appel à la libft directement dans le github (si je fais ça je dois aussi changer la dest dans le makefile de printf))
+- Inclure GNL de Lucas dans ma Libft
+	+ changer la dest du makefile après (voir pour faire appel à la libft directement dans le github (si je fais ça je dois aussi changer la dest dans le makefile de printf))
 
 - Voir comment utiliser le checker
 - Me renseigner sur les listes chainées (voir si c'et completement necessaire pour le tri)
-    - Pour une liste chainée il faut d'abord savoir ce qu'est une structure 
+	- Pour une liste chainée il faut d'abord savoir ce qu'est une structure
 - Créer le makefile
 - Créer le fichier .h
 - Faire le code
-    - Créer les stack A et B
-    - Récupérer les variables et créer une liste chainée (voir comment faire et le principe de fonctionnement)
-    - Mettre l'ensemble des variables dans le stack A pour commencer (possible de d'or et déjà les prétrier? (à voir comment le checker fonctionne))
-    - Créer une fonction pour gérer les erreurs et free aux cas ou
-    - Une fonction pour faire les différentes oppérations (ap etc)
-    - Une fonction pour trouver le chiffre le plus petit et le plus grand
+	- Créer les stack A et B
+	- Récupérer les variables et créer une liste chainée (voir comment faire et le principe de fonctionnement)
+	- Mettre l'ensemble des variables dans le stack A pour commencer (possible de d'or et déjà les prétrier? (à voir comment le checker fonctionne))
+	- Créer une fonction pour gérer les erreurs et free aux cas ou
+	- Une fonction pour faire les différentes oppérations (ap etc)
+	- Une fonction pour trouver le chiffre le plus petit et le plus grand
 - Choisir avec quel type d'alghorytme de tri je vais utiliser
 - Faire passer les variables dans l'algo
 - Done
@@ -31,10 +44,26 @@ Ce que je dois faire pour le projet :
 
 #include "push_swap.h"
 
-int main(void)
+void	init_stack(t_stack *stack)
 {
-	int a[] = {0, 2, 3, 4, 5, 6, 7, 8, 9, 1};
+	stack->top = NULL;
+}
 
-    printf("%d\n", a[1]);
-    return (0);
+int	main(void)
+{
+	int a = 4;
+	int b = 2;
+	int c = 1;
+	int d = 0;
+	int e = 3;
+	int f = 5;
+
+	t_stack	stack_a;
+	t_stack	stack_b;
+
+	init_stack(&stack_a);
+	init_stack(&stack_b);	
+
+	ft_printf("%d\n", a[1]);
+	return (0);
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,5 +6,17 @@
 # include <unistd.h>
 # include "libft/libft.h"
 
+typedef struct s_node
+{
+    int value;
+    struct s_node *next;
+} t_list;
+
+typedef struct s_stack
+{
+    t_list *top;
+} t_stack;
+
+void    init_stack(t_stack *top);
 
 #endif
