5610cdb59b1fc2ea45400032c1dcf5c7b5c35740
Paul Lerick <plerick@c2r4p3.42lehavre.fr>
2025-03-22 20:30:32 +0100

    test more

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,37 +6,64 @@
 /*   By: plerick <plerick@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 15:40:17 by plerick           #+#    #+#             */
-/*   Updated: 2025/03/20 18:22:02 by plerick          ###   ########.fr       */
+/*   Updated: 2025/03/22 20:30:16 by plerick          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+
+
 int	main(void)
 {
-	int	id;
-	int	n;
-	int	i = 1;
+	// int	n;
+	// int	i = 1;
 
-	id = fork();
-	// ft_printf("id est : %d \n", id);
-	if(id == 0)
-	{
-		// ft_printf("coucou du child, mon ID est : %d\n", id);
-		n = 1;
-	}		
-	else
-	{
-		// ft_printf("coucou du parent, mon ID est : %d\n", id);
-		n = 5;
-	}
-	i = n;
-	while (i + 5 > n)
-	{
-		ft_printf("%d ", n);
-		n++;
-	}
-	if(id != 0)
-		wait(0);
+	// id = fork();
+	// // ft_printf("id est : %d \n", id);
+	// if(id == 0)
+	// {
+	// 	// ft_printf("coucou du child, mon ID est : %d\n", id);
+	// 	n = 1;
+	// }		
+	// else
+	// {
+	// 	// ft_printf("coucou du parent, mon ID est : %d\n", id);
+	// 	n = 5;
+	// }
+	// i = n;
+	// while (i + 5 > n)
+	// {
+	// 	ft_printf("%d ", n);
+	// 	n++;
+	// }
+	// if(id != 0)
+	// 	wait(0);
+	// return (0);
+
+	// int	id_1;
+	// int	id_2;
+	// // int	parent_id;
+	
+	// // parent_id = getppid;
+	// // if(argc != 5)
+	// // 	return (0);
+	// id_1 = fork();
+	// id_2 = -1;
+	// if (id_1 != 0)
+	// 	id_2 = fork();
+	// if (id_1 == 0)
+	// 	ft_printf("id_1 = %d\n", getpid);
+	// else if (id_2 == 0)
+	// 	ft_printf("id_2 = %d\n", getpid);
+	// else
+	// {
+	// 	ft_printf("parent ID = %d\n", getppid);
+	// 	while(wait(NULL) != -1 || errno != ECHILD)
+	// 		printf("waited\n");
+	// }
+
+
+
 	return (0);
 }	
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: plerick <plerick@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 15:41:01 by plerick           #+#    #+#             */
-/*   Updated: 2025/03/20 18:15:25 by plerick          ###   ########.fr       */
+/*   Updated: 2025/03/22 18:22:13 by plerick          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 # include <unistd.h>
 # include <string.h>
 # include <stdio.h>
-# include <time.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <errno.h>
 
 #endif
