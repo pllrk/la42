7e49393b9238a5ece54d436f278d81619a889f02
Paul Lerick <plerick@c1r4p8.42lehavre.fr>
2025-03-24 19:42:50 +0100

    comprehension done, start code

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,82 +6,91 @@
 /*   By: plerick <plerick@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 15:40:17 by plerick           #+#    #+#             */
-/*   Updated: 2025/03/22 20:30:16 by plerick          ###   ########.fr       */
+/*   Updated: 2025/03/24 19:42:15 by plerick          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-
-
-int	main(void)
+void handling_error(void)
 {
-	// int	n;
-	// int	i = 1;
+	perror("failed");
+	exit(EXIT_FAILURE);
+	return ;
+}
 
-	// id = fork();
-	// // ft_printf("id est : %d \n", id);
-	// if(id == 0)
-	// {
-	// 	// ft_printf("coucou du child, mon ID est : %d\n", id);
-	// 	n = 1;
-	// }		
-	// else
-	// {
-	// 	// ft_printf("coucou du parent, mon ID est : %d\n", id);
-	// 	n = 5;
-	// }
-	// i = n;
-	// while (i + 5 > n)
-	// {
-	// 	ft_printf("%d ", n);
-	// 	n++;
-	// }
-	// if(id != 0)
-	// 	wait(0);
-	// return (0);
+int child1(int argc, char **argv, char **env, int fd[])
+{
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
+	close(fd[1]);
+}
 
-	// int	id_1;
-	// int	id_2;
-	// // int	parent_id;
-	
-	// // parent_id = getppid;
-	// // if(argc != 5)
-	// // 	return (0);
-	// id_1 = fork();
-	// id_2 = -1;
-	// if (id_1 != 0)
-	// 	id_2 = fork();
-	// if (id_1 == 0)
-	// 	ft_printf("id_1 = %d\n", getpid);
-	// else if (id_2 == 0)
-	// 	ft_printf("id_2 = %d\n", getpid);
-	// else
-	// {
-	// 	ft_printf("parent ID = %d\n", getppid);
-	// 	while(wait(NULL) != -1 || errno != ECHILD)
-	// 		printf("waited\n");
-	// }
+int child2(int argc, char **argv, char **env, int fd[])
+{
+	dup2(fd[1], STDOUT_FILENO);
+	close(fd[0]);
+	close(fd[1]);
+}
 
+int	main(int argc, char **argv, char **env)
+{
 	int fd[2];
-	pip_t pid;
+	pid_t pid1;
+	pid_t pid2;
 	
 	if (pipe(fd) == -1)
+		handling_error;
+	pid1 = fork();
+	if (pid1 < 0)
+		handling_error;
+	if (pid1 == 0)
+		child1(argc, argv, env, fd);
+	else
 	{
-		perror("pipe");
-		exit(EXIT_FAILURE);
+		pid2 = fork();
+		if (pid2 < 0)
+			handling_error;
+		if (pid2 == 0)
+			child2(argc, argv, env, fd);
 	}
-	pid = fork();
-	if (pid < 0)
+	if (pid1 != 0 && pid2 != 0)
 	{
-		perror("fork");
-		exit(EXIT_FAILURE);
+		waitpid(pid1, NULL, 0);
+		waitpid(pid2, NULL, 0);
 	}
-	if (pid == 0)
-	{
-		
-	}
-
 
 	return (0);
+
+
+	int		i = 0;
+	int		j = 0;
+	char	**path;
+	
+	while(env[i])
+	{
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
+		{
+			path = ft_split(env[i]+5, ':');
+			while (path[j])
+			{
+				printf("%s\n", path[j]);
+				j++;
+			}
+		}
+		i++;
+	}
+
+	// (void)env;
+	// int	i = 0;
+	// char **cmd;
+	// if (argc != 2)
+	// 	return (0);
+	// cmd = ft_split(argv[1], ' ');
+	// while (i < 2)
+	// {
+	// 	printf("%s", cmd[i]);
+	// 	i++;
+	// }
+	// https://github.com/gabcollet/pipex
 }	
--- pipex/utils.c
+++ pipex/utils.c
@@ -1,29 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plerick <plerick@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/20 15:41:01 by plerick           #+#    #+#             */
-/*   Updated: 2025/03/24 19:35:15 by plerick          ###   ########.fr       */
+/*   Created: 2025/03/24 19:34:01 by plerick           #+#    #+#             */
+/*   Updated: 2025/03/24 19:34:02 by plerick          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
-
-# include "libft/libft.h"
-# include <unistd.h>
-# include <string.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <sys/wait.h>
-# include <errno.h>
-
-int child1(int argc, char **argv, char **env, int fd[]);
-int child2(int argc, char **argv, char **env, int fd[]);
-void handling_error(void);
-
-
-#endif
