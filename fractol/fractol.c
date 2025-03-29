14ed48aad5adb8a6a8b844ffeefd102f071a61b4
root <root@BonPC>
2025-03-29 20:31:25 +0100

    creation of main etc

---
+++ fractol/fractol.c
@@ -0,0 +1,8 @@
+
+
+#include "fractol.h"
+
+int	main(int argc, char **argv)
+{
+	aze
+}
---
+++ fractol/fractol.h
@@ -0,0 +1,11 @@
+
+
+#ifndef	FRACTOL_H
+# define FRACTOL_H
+
+# include "libft/libft.h"
+# include <unistd.h>
+# include "mlx_int.h"
+# include "mlx.h"
+
+#endif
